eab7bc92248eb9f52985c6177782b30e9d32d217
ngjohn0996-glitch <ngjohn0996@gmail.com>
2026-04-01 17:06:01 +0800

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def backtrack(index, list_01):
+            #3)run untill last_index of list
+            if index >= len(nums):
+                result.append(list_01.copy())
+                return
+
+
+            #1)append curr_index into list
+            list_01.append(nums[index])
+            #2)using updated_index run_action_#1) 
+            backtrack(index+1, list_01)
+            #3.1)pop last/latest element
+            list_01.pop()
+            #4)using updated_index run_action_#1) 
+            #5)index out of range of list, return
+            backtrack(index+1, list_01)
+
+
+            #6)continue parent_second_call / second_last_index, run untill all combination
+        
+        backtrack(0, [])
+        return result
