f196045155167e8369e344676081654362a04d06
ngjohn0996-glitch <ngjohn0996@gmail.com>
2026-04-18 18:36:33 +0800

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        #memorization-solution
+        dict_01 = {1 : 1, 2 : 2}   #stair-ways dict
+
+        def ways(n):
+            if n in dict_01:
+                #2)run untill stair=3
+                return dict_01[n]
+
+
+            else:
+                #1)find prev_stair
+                #2.1)find prev_stair's ways & prev_prev_stair's ways (plus 1 / 2 step to curr_stair) 
+                prev_stair = ways(n - 1)
+                prev_prev_stair = ways(n - 2)
+                #3)assign curr_stair-ways 
+                dict_01[n] = prev_stair + prev_prev_stair 
+
+
+                #4)pass sum_ways to parent_call >> continue parent_second_call, run untill DFS_last_node
+                return dict_01[n] 
+
+        return ways(n)
