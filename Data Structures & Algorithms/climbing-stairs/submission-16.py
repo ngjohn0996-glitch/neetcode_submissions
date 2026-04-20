f3ae9c6cde42c54ab1501b0d4496c686893499c9
ngjohn0996-glitch <ngjohn0996@gmail.com>
2026-04-21 01:33:13 +0800

    Add: climbing-stairs - submission-16

---
+++ Data Structures & Algorithms/climbing-stairs/submission-16.py
@@ -0,0 +1,46 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        '''#memorization-solution
+        dict_01 = {1 : 1, 2 : 2}   #stair-ways dict
+
+        def ways(n):
+            if n in dict_01:
+                #2)run untill stair=3
+                return dict_01[n]
+
+
+            else:
+                #1)find prev_stair
+                #2.1)find prev_stair's ways & prev_prev_stair's ways (plus 1/2 step to curr_stair) (prev_prev_prev_stair >> overlap_w_prev_stair/prev_prev_stair)
+                prev_stair = ways(n - 1)
+                prev_prev_stair = ways(n - 2)
+                #3)assign curr_stair-sum_ways 
+                dict_01[n] = prev_stair + prev_prev_stair 
+
+
+                #4)pass sum_ways to parent_call >> continue parent_second_call, run untill DFS_last_node
+                return dict_01[n] 
+
+        return ways(n)'''
+
+        #edge-case
+        if n == 0:
+            return 1
+        if n == 1:
+            return 1
+        #tabulation-solution
+        prev_node, prev_prev_node = 1, 1
+
+        #1)start_new_table index=2/second_stair
+        for index in range(2, n+1):
+            prev = prev_node   #4.1)before prev_node assign prev_node.next, prev_node is prev_node
+            
+            #2)find curr_node(prev_stair's ways + prev_prev_stair's ways)
+            curr_node = prev_node + prev_prev_node 
+            #3)prev_node move to next_node
+            prev_node = curr_node
+            #4)prev_prev_node move to next_node
+            prev_prev_node = prev 
+
+        return curr_node
+
