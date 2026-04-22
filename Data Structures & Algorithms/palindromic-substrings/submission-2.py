8fc4e4c4985c06b0a977387592be9c9de5ab4f0f
ngjohn0996-glitch <ngjohn0996@gmail.com>
2026-04-22 17:30:26 +0800

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        result = 0
+
+        #1)loop through element
+        for index in range(len(s)):
+            #2)two_pointer at element (odd_length)
+            point_01, point_02 = index, index
+
+            #3)check if palindromic  
+            while (point_01 >= 0 and point_02 < len(s) and 
+                   s[point_01] == s[point_02]):
+                #4)update result
+                result += 1
+                #4)always update two_pointer
+                point_01 -= 1
+                point_02 += 1
+
+
+            #2)two_pointer at element & element's right (even_length)
+            point_01, point_02 = index, index+1
+                   
+            #3)check if palindromic  
+            while (point_01 >= 0 and point_02 < len(s) and 
+                   s[point_01] == s[point_02]):
+                #4)update result
+                result += 1
+                #4)always update two_pointer
+                point_01 -= 1
+                point_02 += 1
+        return result
