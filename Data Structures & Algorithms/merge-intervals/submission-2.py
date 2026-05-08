95ae3a9c924821778004c408818c950c4f8bedd0
ngjohn0996-glitch <ngjohn0996@gmail.com>
2026-05-09 01:53:06 +0800

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x : x[0])
+        result = [intervals[0]]
+
+        #1)loop through intervals[start at index=1]
+        for curr_interval_start, curr_interval_end in intervals[1:]:
+            prev_interval = result[-1]
+
+            #2)if previous_interval's end smaller_than curr_interval's start
+            if prev_interval[1] < curr_interval_start:
+                #2.1)curr_interval append into result
+                result.append([curr_interval_start, curr_interval_end])
+
+            #2)if previous_interval is overlapping curr_interval
+            else:
+                #2.1)merge two_interval into previous_interval, previous_interval compare with next_interval
+                prev_interval[1] = max(prev_interval[1], curr_interval_end)
+
+        return result
