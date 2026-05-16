cae2b41872933ea30d3c53ae6759744f05a8cb7f
ngjohn0996-glitch <ngjohn0996@gmail.com>
2026-05-16 18:50:47 +0800

    Add: meeting-schedule-ii - submission-14

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-14.py
@@ -0,0 +1,35 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        curr_start = sorted([interval.start for interval in intervals])  
+        curr_end = sorted([interval.end for interval in intervals])
+        start_index, end_index = 0, 0
+        room, max_rooms = 0, 0
+
+        #1)curr_start, curr_end
+        while start_index < len(curr_start):
+
+            #2)if curr_start smaller than curr_end
+            if curr_start[start_index] < curr_end[end_index]:
+                #2.1)interval+1 / overlapping_interval+1
+                room += 1
+                #2.2)curr_start move to next_start(check if next_start's interval overlap with curr_end's interval)
+                start_index += 1
+                max_rooms = max(max_rooms, room)
+
+            #3)if curr_start larger_than curr_end
+            else:
+                #3.1)interval-1 / overlapping_interval-1
+                #3.1)find interval overlap with curr_start's interval
+                #3.1)curr_end's interval cant_overlap curr_start's interval(curr_end's interval align with curr_start's interval)
+                room -= 1
+                #3.2)curr_end move to next_end(curr_start smaller than curr_end to find interval overlap with curr_start's interval) 
+                end_index += 1 
+        return max_rooms
