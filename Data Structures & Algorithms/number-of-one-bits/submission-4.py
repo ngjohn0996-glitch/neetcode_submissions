be250d982184d84b0659dbb33b2c6faeec98b9a5
ngjohn0996-glitch <ngjohn0996@gmail.com>
2026-05-22 03:08:24 +0800

    Add: number-of-one-bits - submission-4

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        result = 0
+
+        '''#1)loop untill bit == 0
+        while n != 0:
+            #2)1  bitwise_and  last_element_of_bit
+            res = 1 & n
+            result += res
+            #3)bit right_shift by 1
+            n = n >> 1
+        return result'''
+
+        #1)loop untill bit == 0
+        while n != 0:   
+            #2)remove rightmost 1
+            #2)bit  bitwise_and  bit bitwise_sum two_complement_of_1
+            remove = n & (n-1) 
+            #3)update bit
+            n = remove
+            result += 1
+        return result
