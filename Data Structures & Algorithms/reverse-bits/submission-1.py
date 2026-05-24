b3f203fc8ad7a6af97fda69163d272b73ef4ad45
ngjohn0996-glitch <ngjohn0996@gmail.com>
2026-05-24 18:07:18 +0800

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+
+        #1)loop through i(imagine 5)
+        for i in range(32):
+            
+            #2)n's_bit rightmost_i_element
+            rightmost_i_element = (n >> i) & 1
+            #3)result's_bit leftmost_i_element
+            leftmost_i_element = rightmost_i_element << (31 - i) 
+            #4)prev_result's_bit  combine  result's_bit leftmost_i_element
+            combine = result | leftmost_i_element
+            result = combine
+
+        return result
