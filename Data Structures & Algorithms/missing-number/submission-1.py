da19843424e11cd710fadf40a9269eb8487249fb
ngjohn0996-glitch <ngjohn0996@gmail.com>
2026-05-25 19:16:16 +0800

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    #bitwise_xor can change order (a ^ b ^ c = a ^ c ^ b)
+    def missingNumber(self, nums: List[int]) -> int:
+        prev_xor = 0
+
+        #1)loop through expect, actual
+        for expect in range(len(nums)):
+            #2)prev_xor  bitwise_xor  expect  bitwise_xor  actual
+            curr_xor = prev_xor ^ expect ^ nums[expect]
+            prev_xor = curr_xor
+
+        #3)all_expect  bitwise_xor  all_actual
+        #3.1)prev_xor  bitwise_xor  final_expect
+        result = prev_xor ^ len(nums)
+        return result
